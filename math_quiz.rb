a95a6d6964747dd040636e5c9b63420e5e954b97
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-08 11:57:09 -0500

    Implement MathQuiz logic with random numbers

--- math_quiz.rb
+++ math_quiz.rb
@@ -1,3 +1,11 @@
 class MathQuiz
-  
+  attr_reader :question
+  attr_reader :answer
+
+  def initialize
+    first_num = rand(100)
+    second_num = rand(100)
+    @question = "What is #{first_num} + #{second_num}"
+    @answer = first_num + second_num
+  end
 end
